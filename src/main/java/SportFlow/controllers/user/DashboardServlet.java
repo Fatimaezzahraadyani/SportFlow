6a5755dd1cd885070c56f002849b39c57eb3cf2f
fatimaezzahraadyani <fatimaezzahraadyani@gmail.com>
2025-03-14 10:04:59 +0100

    modifier des erreurs dans jsp files et ajouter dashboard

---
+++ src/main/java/SportFlow/controllers/user/DashboardServlet.java
@@ -0,0 +1,25 @@
+package SportFlow.controllers.user;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/dashboard")
+public class DashboardServlet extends HttpServlet {
+    public void init () {}
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException
+    {
+
+        RequestDispatcher rs = req.getRequestDispatcher("/views/user/dashboard.jsp");
+        rs.forward(req, res);
+
+    }
+
+}
--- src/main/webapp/views/parcials/footer.jsp
+++ src/main/webapp/views/parcials/footer.jsp
@@ -47,7 +47,7 @@
   <!-- footer section -->
   <footer class="container-fluid footer_section">
     <p>
-      &copy; 2020 All Rights Reserved. Design by
+      &copy; 2025 All Rights Reserved. Design by
       <a href="https://html.design/">Free Html Templates</a>
     </p>
   </footer>
